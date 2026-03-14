ba75f7f5c3f3ce0150a7ab60fca8357d9199de20
Filipe Toscano <filipe.toscano@gmail.com>
2026-03-14 18:10:12 +0400

    Update descriptions for correctness

--- src/Andalus.Cryptography/KeyType.cs
+++ src/Andalus.Cryptography/KeyType.cs
@@ -3,32 +3,41 @@
 /// <summary />
 public enum KeyType
 {
-    /// <summary />
+    /// <summary>
+    /// RSA, with 2048 bit key size
+    /// </summary>
     Rsa2048 = 1,
 
-    /// <summary />
+    /// <summary>
+    /// RSA, with 3072 bit key size
+    /// </summary>
     Rsa3072,
 
-    /// <summary />
+    /// <summary>
+    /// RSA, with 4096 bit key size
+    /// </summary>
     Rsa4096,
 
     /// <summary>
-    /// NIST Prime 256
+    /// ECDsa, with NIST Prime 256 curve
     /// </summary>
     EcdsaP256,
 
     /// <summary>
-    /// NIST Prime 384
+    /// ECDsa, with NIST Prime 384 curve
     /// </summary>
     EcdsaP384,
 
     /// <summary>
-    /// NIST Prime 521
+    /// ECDsa, with NIST Prime 521 curve
     /// </summary>
     EcdsaP521,
 
     /// <summary>
-    /// Standards for Efficient Cryptography
+    /// ECDsa, with SECP K1 curve
     /// </summary>
+    /// <remarks>
+    /// SECP = Standards for Efficient Cryptography
+    /// </remarks>
     EcdsaSecp256k1,
 }
