a8e67078803ae57b6e71d155737429f4555e27d3
Filipe Toscano <filipe.toscano@gmail.com>
2026-03-14 13:04:46 +0400

    Add basic `key` command

---
+++ tools/Andalus.Cli/KeyCommand.cs
@@ -0,0 +1,30 @@
+﻿using Andalus.Cryptography;
+using McMaster.Extensions.CommandLineUtils;
+
+namespace Andalus.Cli;
+
+/// <summary />
+[Command( "key" )]
+public class KeyCommand
+{
+    /// <summary />
+    public async Task<int> OnExecuteAsync()
+    {
+        ICryptoProvider cp = new FileCryptoProvider( new FileCryptoProviderOptions()
+        {
+            RootDirectory = Path.Combine( Environment.CurrentDirectory, "key-vault" ),
+        } );
+
+        var kp = await cp.CreateKeyPairAsync( new KeyCreationOptions()
+        {
+            KeyName = Guid.NewGuid().ToString(),
+            KeyType = KeyType.EcdsaSecp256k1,
+            Exportable = true,
+        } );
+
+        Console.WriteLine( kp.KeyId );
+        Console.WriteLine( kp.KeyType );
+
+        return 0;
+    }
+}
