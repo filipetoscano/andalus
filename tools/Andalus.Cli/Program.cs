5744e5074c09b9aa7ee48f30d8010ee6f9ecdf6a
Filipe Toscano <filipe.toscano@gmail.com>
2026-03-14 12:37:13 +0400

    Structure for CLI

--- tools/Andalus.Cli/Program.cs
+++ tools/Andalus.Cli/Program.cs
@@ -1,2 +1,80 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine( "Hello, World!" );
+﻿using McMaster.Extensions.CommandLineUtils;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Andalus.Cli;
+
+/// <summary />
+[Command( "andalus", Description = "" )]
+[VersionOptionFromMember( MemberName = nameof( GetVersion ) )]
+public class Program
+{
+    /// <summary />
+    public static int Main( string[] args )
+    {
+        /*
+         * 
+         */
+        var app = new CommandLineApplication<Program>();
+
+        var svc = new ServiceCollection();
+        svc.AddOptions();
+
+        var sp = svc.BuildServiceProvider();
+
+
+        /*
+         * 
+         */
+        try
+        {
+            app.Conventions
+                .UseDefaultConventions()
+                .UseConstructorInjection( sp );
+        }
+        catch ( Exception ex )
+        {
+            Console.WriteLine( "ftl: unhandled exception during setup" );
+            Console.WriteLine( ex.ToString() );
+
+            return 2;
+        }
+
+
+        /*
+         * 
+         */
+        try
+        {
+            return app.Execute( args );
+        }
+        catch ( UnrecognizedCommandParsingException ex )
+        {
+            Console.WriteLine( "err: " + ex.Message );
+
+            return 2;
+        }
+        catch ( Exception ex )
+        {
+            Console.WriteLine( "ftl: unhandled exception during execution" );
+            Console.WriteLine( ex.ToString() );
+
+            return 2;
+        }
+    }
+
+
+    /// <summary />
+    private static string GetVersion()
+    {
+        return typeof( Program ).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()!.InformationalVersion;
+    }
+
+
+    /// <summary />
+    public int OnExecute( CommandLineApplication app )
+    {
+        app.ShowHelp();
+        return 1;
+    }
+}
