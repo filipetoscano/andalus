76ae39d858f9ea877e2d8f45f9cc050eeebd508e
Filipe Toscano <filipe.toscano@gmail.com>
2026-03-17 18:21:21 +0400

    Add memory crypto provider, for unit tests

---
+++ src/Andalus.Cryptography/MemoryCryptoProvider.cs
@@ -0,0 +1,11 @@
+﻿namespace Andalus.Cryptography;
+
+/// <summary />
+public class MemoryCryptoProvider : KeyStoreCryptoProvider, ICryptoProvider
+{
+    /// <summary />
+    public MemoryCryptoProvider()
+        : base( new MemoryKeyStore() )
+    {
+    }
+}
---
+++ src/Andalus.Cryptography/MemoryKeyStore.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Concurrent;
+
+namespace Andalus.Cryptography;
+
+/// <summary />
+public class MemoryKeyStore : IKeyStore
+{
+    private readonly ConcurrentDictionary<string, KeypairRecord> _store;
+
+
+    /// <summary />
+    public MemoryKeyStore()
+    {
+        _store = new ConcurrentDictionary<string, KeypairRecord>();
+    }
+
+
+    /// <inheritdoc />
+    public Task CreateAsync( KeypairRecord record, CancellationToken cancellationToken )
+    {
+        _store.AddOrUpdate( record.KeyId, record, ( k, v ) =>
+        {
+            record.MomentCreated = v.MomentCreated;
+
+            return record;
+        } );
+
+        return Task.CompletedTask;
+    }
+
+
+    /// <inheritdoc />
+    public Task<RemoveResult> RemoveAsync( string keyId, CancellationToken cancellationToken )
+    {
+        _store.Remove( keyId, out _ );
+
+        return Task.FromResult( new RemoveResult() );
+    }
+
+
+    /// <inheritdoc />
+    public Task<KeypairRecord> RetrieveAsync( string keyId, CancellationToken cancellationToken )
+    {
+        var v = _store.GetValueOrDefault( keyId );
+
+        if ( v == null )
+            throw new KeyNotFoundException( $"Key '{keyId}' not found." );
+
+        return Task.FromResult( v );
+    }
+}
