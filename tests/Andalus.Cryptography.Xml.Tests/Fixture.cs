631344ce1e684b70841ceeeb0217a87ad4a8f722
Filipe Toscano <filipe.toscano@gmail.com>
2026-03-17 18:22:16 +0400

    Add basic unit test

---
+++ tests/Andalus.Cryptography.Xml.Tests/Fixture.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+
+namespace Andalus.Cryptography.Xml.Tests;
+
+/// <summary />
+public class Fixture : IAsyncLifetime
+{
+    /// <summary />
+    public ServiceProvider Services { get; private set; } = null!;
+
+    /// <summary />
+    public KeyReference EcdsaKey { get; set; } = null!;
+
+    /// <summary />
+    public KeyReference RsaKey { get; set; } = null!;
+
+
+    /// <summary />
+    public async Task InitializeAsync()
+    {
+        this.Services = SetupDependencyInjection();
+
+        var cp = Services.GetRequiredService<ICryptoProvider>();
+
+        this.EcdsaKey = await cp.CreateKeyPairAsync( new KeyCreationOptions()
+        {
+            KeyName = "ECDSA",
+            KeyType = KeyType.EcdsaSecp256k1,
+        } );
+
+        this.RsaKey = await cp.CreateKeyPairAsync( new KeyCreationOptions()
+        {
+            KeyName = "RSA",
+            KeyType = KeyType.Rsa2048,
+        } );
+    }
+
+
+    /// <summary />
+    public async Task DisposeAsync()
+    {
+        if ( Services is IAsyncDisposable ad )
+            await ad.DisposeAsync();
+        else
+            Services?.Dispose();
+    }
+
+
+    /// <summary />
+    protected ServiceProvider SetupDependencyInjection()
+    {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .MinimumLevel.Override( "Microsoft", LogEventLevel.Warning )
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .CreateLogger();
+
+        var svc = new ServiceCollection();
+
+        svc.AddLogging( b =>
+        {
+            b.ClearProviders();
+            b.AddSerilog( Log.Logger, dispose: true );
+        } );
+
+        svc.AddOptions();
+        svc.AddSingleton<ICryptoProvider, MemoryCryptoProvider>();
+
+        return svc.BuildServiceProvider();
+    }
+}
---
+++ tests/Andalus.Cryptography.Xml.Tests/XmlDigSigTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Security.Cryptography;
+using System.Xml;
+
+namespace Andalus.Cryptography.Xml.Tests;
+
+/// <summary />
+public class XmlDigSigTests : IClassFixture<Fixture>
+{
+    private readonly Fixture _f;
+    private readonly ICryptoProvider _cp;
+
+
+    /// <summary />
+    public XmlDigSigTests( Fixture fixture )
+    {
+        _f = fixture;
+        _cp = fixture.Services.GetRequiredService<ICryptoProvider>();
+    }
+
+
+    /// <summary />
+    [Theory]
+    [InlineData( SignatureType.Enveloped )]
+    [InlineData( SignatureType.Enveloping )]
+    public void Roundtrip( SignatureType signatureType )
+    {
+        var doc = new XmlDocument() { PreserveWhitespace = true };
+        doc.LoadXml( @"<root>
+    <another />
+    <bites>
+        <the />
+        <dust />
+    </bites>
+</root>" );
+
+        var signed = XmlDigSig.Sign( signatureType, doc, _cp, _f.EcdsaKey, HashAlgorithmName.SHA256, new XmlDigSigOptions()
+        {
+        } );
+
+        XmlDigSig.VerifyAll( signed );
+    }
+}
