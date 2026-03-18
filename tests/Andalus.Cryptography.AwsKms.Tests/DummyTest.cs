8d3059f1ac041f88677f74d2d092a17bfce8ef28
Filipe Toscano <filipe.toscano@gmail.com>
2026-03-18 22:00:53 +0400

    Add placeholder Aws/Google KMS test projects

---
+++ tests/Andalus.Cryptography.AwsKms.Tests/DummyTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Andalus.Cryptography.AwsKms.Tests;
+
+/// <summary />
+public class DummyTest
+{
+    /// <summary />
+    [Fact]
+    public void Dummy()
+    {
+    }
+}
---
+++ tests/Andalus.Cryptography.GoogleKms.Tests/DummyTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Andalus.Cryptography.GoogleKms.Tests;
+
+/// <summary />
+public class DummyTest
+{
+    /// <summary />
+    [Fact]
+    public void Dummy()
+    {
+    }
+}
