c2be95b2e6dc497ee9fbccb6c144fa2da388aaa3
Filipe Toscano <filipe.toscano@gmail.com>
2026-03-23 14:01:02 +0400

    Add missing algorithm URI, move to remarks

--- src/Andalus.Cryptography.Xml/XmlCanonicalization.cs
+++ src/Andalus.Cryptography.Xml/XmlCanonicalization.cs
@@ -8,38 +8,51 @@ public enum XmlCanonicalization
 {
     /// <summary>
     /// Canonical XML 1.0 (omits comments).
-    /// <para>Algorithm URI: http://www.w3.org/TR/2001/REC-xml-c14n-20010315</para>
     /// </summary>
+    /// <remarks>
+    /// Algorithm URI: <c>http://www.w3.org/TR/2001/REC-xml-c14n-20010315</c>
+    /// </remarks>
     XmlDsigC14NTransform,
 
     /// <summary>
     /// Canonical XML 1.1 (omits comments).
-    /// <para>Algorithm URI: http://www.w3.org/TR/2001/REC-xml-c14n-20010315</para>
     /// </summary>
+    /// <remarks>
+    /// Algorithm URI: <c>http://www.w3.org/2006/12/xml-c14n11</c>
+    /// </remarks>
     XmlDsigC14N11Transform,
 
     /// <summary>
     /// Canonical XML 1.0 (preserves comments).
-    /// <para>Algorithm URI: http://www.w3.org/TR/2001/REC-xml-c14n-20010315#WithComments</para>
     /// </summary>
+    /// <remarks>
+    /// Algorithm URI: <c>http://www.w3.org/TR/2001/REC-xml-c14n-20010315#WithComments</c>
+    /// </remarks>
     XmlDsigC14NWithCommentsTransform,
 
     /// <summary>
     /// Canonical XML 1.1 (preserves comments).
     /// </summary>
+    /// <remarks>
+    /// Algorithm URI: <c>http://www.w3.org/2006/12/xml-c14n11#WithComments</c>
+    /// </remarks>
     XmlDsigC14N11WithCommentsTransform,
 
     /// <summary>
     /// Exclusive Canonical XML 1.0 (omits comments). Only namespaces
     /// visibly utilized by the target node set are included.
-    /// <para>Algorithm URI: http://www.w3.org/2001/10/xml-exc-c14n#</para>
     /// </summary>
+    /// <remarks>
+    /// Algorithm URI: <c>http://www.w3.org/2001/10/xml-exc-c14n#</c>
+    /// </remarks>
     XmlDsigExcC14NTransform,
 
     /// <summary>
     /// Exclusive Canonical XML 1.0 (preserves comments). Only namespaces
     /// visibly utilized by the target node set are included.
-    /// <para>Algorithm URI: http://www.w3.org/2001/10/xml-exc-c14n#WithComments</para>
     /// </summary>
+    /// <remarks>
+    /// Algorithm URI: <c>http://www.w3.org/2001/10/xml-exc-c14n#WithComments</c>
+    /// </remarks>
     XmlDsigExcC14NWithCommentsTransform,
 }
